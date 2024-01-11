c6777697ca694276081ea8033666552ccfbdb5d7
xiejunyc <156200893+xiejunyc@users.noreply.github.com>
2024-01-11 18:34:33 +0800

    Update adbyby_host.sh

--- trunk/user/adbyby/adbyby_host.sh
+++ trunk/user/adbyby/adbyby_host.sh
@@ -1,4 +1,4 @@
 #一行一个地址，不需要的在前面加 "#"号注释，后台会自动下载，合并，去重。不建议太多，以免误杀严重！
 #此hosts规则是加载到dnsmasq的。
-#yhost
-https://raw.githubusercontent.com/VeleSila/yhosts/master/hosts.txt
+#10007 host规则地址：https://www.gitlink.org.cn/keytoolazy/10007_auto
+https://raw.gitmirror.com/lingeringsound/10007_auto/master/adb.txt
